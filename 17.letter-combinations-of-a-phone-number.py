c900da4b5c8754b5733fba66a1bfdad0899a4a7a
Li, Wanjun <wli626@gatech.edu>
2021-04-18 00:37:46 -0400

    Create 17.letter-combinations-of-a-phone-number.py

---
+++ 17.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=17 lang=python3
+#
+# [17] Letter Combinations of a Phone Number
+#
+
+# @lc code=start
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+        
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        combinations = list()
+        backtrack(0)
+        return combinations
+
+# 时间复杂度：O(3^m * 4^n), 其中 m是输入中对应 3 个字母的数字个数，n 是输入中对应 4 个字母的数字个数
+
+# 空间复杂度：O(m+n)，m+n是输入数字的总个数。除了返回值以外，
+# 空间复杂度主要取决于哈希表以及回溯过程中的递归调用层数，哈希表的大小与输入无关，可以看成常数，递归调用层数最大为 m+n
+
+
+        
+# @lc code=end
+
